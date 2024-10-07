7ae873249d1800381cb6792a6120b017c074e54d
Luis Henrique <110108496+LuisHenriqueGavaJunior@users.noreply.github.com>
2024-10-07 14:01:32 +0000

    editora e aula 04/10

---
+++ app/src/main/java/application/model/Editora.java
@@ -0,0 +1,39 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "editoras")
+public class Editora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cidade;
+
+    @ManyToOne
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public long getId() {
+        return this.id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getCidade() {
+        return this.cidade;
+    }
+}
